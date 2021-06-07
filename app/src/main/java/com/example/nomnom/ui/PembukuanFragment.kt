09928f9f51e14956ef0d6333c7de8d35b88ebb9f
salomohutapea <hutapeasalomo@gmail.com>
2021-06-07 18:00:35 +0700

    Add webview for pembukuan

--- app/src/main/java/com/example/nomnom/ui/PembukuanFragment.kt
+++ app/src/main/java/com/example/nomnom/ui/PembukuanFragment.kt
@@ -1,18 +1,16 @@
 package com.example.nomnom.ui
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
 import com.example.nomnom.R
 
 class PembukuanFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,5 +18,14 @@ class PembukuanFragment : Fragment() {
     ): View? {
 
         return inflater.inflate(R.layout.fragment_pembukuan, container, false)
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val myWebView: WebView? = view.findViewById(R.id.webview_pembukuan)
+        myWebView?.webViewClient = WebViewClient()
+        myWebView?.loadUrl("http://cuanki.convuloca.xyz")
+        myWebView?.settings?.javaScriptEnabled = true
     }
 }
